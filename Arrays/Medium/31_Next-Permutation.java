3daa7fe175ab48438633a4223f7cb437ec033950
AKHILESH-create <akhilesh.k19857@gmail.com>
2026-05-31 16:04:01 +0530

    commit changes

---
+++ Arrays/Medium/31_Next-Permutation.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+class Solution {
+
+    public void nextPermutation(int[] nums) {
+
+        int i = nums.length - 2;
+
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+
+        if (i >= 0) {
+            int j = nums.length - 1;
+
+            while (nums[j] <= nums[i]) {
+                j--;
+            }
+
+            swap(nums, i, j);
+        }
+
+        reverse(nums, i + 1);
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    private void reverse(int[] nums, int start) {
+        int end = nums.length - 1;
+
+        while (start < end) {
+            swap(nums, start, end);
+            start++;
+            end--;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Solution sol = new Solution();
+
+        int[] nums = { 1, 2, 3 };
+
+        sol.nextPermutation(nums);
+
+        System.out.println(Arrays.toString(nums));
+    }
+}
