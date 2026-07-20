801e041391e4d41166b2e54a49efe9f1978bb0c0
AKHILESH-create <akhilesh.k19857@gmail.com>
2026-07-20 23:57:20 +0530

    code added

---
+++ Arrays/Easy/1260_Shift-2D-Grid.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<List<Integer>> shiftGrid(int[][] grid, int k) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int total = m * n;
+
+        k %= total;
+
+        List<List<Integer>> ans = new ArrayList<>();
+
+        for (int i = 0; i < m; i++) {
+            List<Integer> row = new ArrayList<>();
+            for (int j = 0; j < n; j++) {
+                int idx = (i * n + j - k + total) % total;
+                int r = idx / n;
+                int c = idx % n;
+                row.add(grid[r][c]);
+            }
+            ans.add(row);
+        }
+
+        return ans;
+    }
+}
