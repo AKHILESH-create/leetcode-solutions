5eff2f8568b7d8a1f7ef06c67e017584d8f82d18
AKHILESH-create <akhilesh.k19857@gmail.com>
2026-08-04 20:29:43 +0530

    code added

---
+++ Arrays/Easy/3731_Find-Missing-Elements.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+class Solution {
+
+    public List<Integer> findMissingElements(int[] nums) {
+        Arrays.sort(nums);
+
+        List<Integer> ans = new ArrayList<>();
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = nums[i] + 1; j < nums[i + 1]; j++) {
+                ans.add(j);
+            }
+        }
+
+        return ans;
+    }
+}
