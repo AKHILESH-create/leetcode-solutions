02a64866d83965a474b8322aaa7ac7d3157c02b8
AKHILESH-create <akhilesh.k19857@gmail.com>
2026-08-06 23:57:34 +0530

    code added

--- Depth-FirstSearch/Medium/3310_Remove-Methods-From-Project.java
+++ Depth-FirstSearch/Medium/3310_Remove-Methods-From-Project.java
@@ -1,3 +1,8 @@
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+
 class Solution {
 
     public List<Integer> remainingMethods(int n, int k, int[][] invocations) {
