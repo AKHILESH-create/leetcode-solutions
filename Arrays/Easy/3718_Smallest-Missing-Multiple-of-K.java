40d927cd4bae695f1ad2d95a3833bebf46b3c40b
AKHILESH-create <akhilesh.k19857@gmail.com>
2026-08-25 23:45:20 +0530

    code added

---
+++ Arrays/Easy/3718_Smallest-Missing-Multiple-of-K.java
@@ -0,0 +1,17 @@
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+
+    public int missingMultiple(int[] nums, int k) {
+        Set<Integer> seen = new HashSet<>();
+        for (int num : nums) {
+            seen.add(num);
+        }
+        int ans = k;
+        while (seen.contains(ans)) {
+            ans += k;
+        }
+        return ans;
+    }
+}
